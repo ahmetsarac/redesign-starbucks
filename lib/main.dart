b67bcb04b467b38c3584c8d365e281281f235c8f
Ahmet Saraç <ahmet.src.99@gmail.com>
2022-03-27 23:04:04 +0300

    Project was initialized.

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RedesignStarbucks());
+}
+
+class RedesignStarbucks extends StatelessWidget {
+  const RedesignStarbucks({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
