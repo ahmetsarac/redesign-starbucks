37c2ace7aeb9055071f7f7db89a3d910bb976274
Ahmet Saraç <ahmet.src.99@gmail.com>
2022-03-27 23:42:41 +0300

    Colors were added.

---
+++ lib/theme/colors.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class PrimaryColors {
+  static const Color darkGreen = Color(0xFF1D4D4F);
+  static const Color mediumGreen = Color(0xFF285F43);
+  static const Color mainGreen = Color(0xFF4AA366);
+  static const Color lightGreen = Color(0xFF5AAE8A);
+  static const Color softGreen = Color(0xFFDDF1E9);
+}
+
+class GreyColors {
+  static const Color dark = Color(0xFF2E2D38);
+  static const Color darkGrey = Color(0xFF4D5661);
+  static const Color grey = Color(0xFF6F8094);
+  static const Color mediumGrey = Color(0xFFC4C4C4);
+  static const Color lightGrey = Color(0xFFDBDBDB);
+  static const Color softGrey = Color(0xFFF4F4F4);
+}
+
+class ErrorColors {
+  static const Color darkRed = Color(0xFF6B171F);
+  static const Color mainRed = Color(0xFFD62E3E);
+  static const Color lightRed = Color(0xFFE26D78);
+  static const Color softRed = Color(0xFFF3C0C5);
+}
+
+class InfoColors {
+  static const Color gold = Color(0xFFC1A063);
+  static const Color yellow = Color(0xFFFFDD2D);
+  static const Color lightYellow = Color(0xFFFEE690);
+  static const Color softYellow = Color(0xFFFFF4CB);
+}
+
+class BlueColors {
+  static const Color darkBlue = Color(0xFF253365);
+  static const Color mainBlue = Color(0xFF2D81FF);
+  static const Color lightBlue = Color(0xFF90A8FE);
+  static const Color softBlue = Color(0xFFCBCDFF);
+}
+
+class OrangeColors {
+  static const Color darkOrange = Color(0xFF934228);
+  static const Color mainOrange = Color(0xFFFF782D);
+  static const Color lightOrange = Color(0xFFFEB190);
+  static const Color softOrange = Color(0xFFFFE1CB);
+}
